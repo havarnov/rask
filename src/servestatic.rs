b06164fb34653d9b61befbb2229adcd7c4410bd4
Håvar Nøvik <havar.novik@gmail.com>
2015-11-07 15:01:53 +0100

    Removed unneccesary '&'.

--- src/servestatic.rs
+++ src/servestatic.rs
@@ -27,9 +27,9 @@ impl ServeStatic {
 
 impl Handler for ServeStatic {
     fn handle(&self, req: &Request, mut res: Response) {
-        let path = match &req.path {
-            &Some(ref path) => path,
-            &None => {
+        let path = match req.path {
+            Some(ref path) => path,
+            None => {
                 res.status(StatusCode::InternalServerError);
                 let _ = res.write_body("Internal 500 error");
                 return;
